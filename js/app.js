9690505708a37d7e41db0cf4b141cd2ed45102d7
Vengaa <luisfelipeadm.ps@gmail.com>
2024-04-22 19:48:55 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,99 @@
+
+var altura = 0
+var largura = 0
+var vidas = 5
+var tempo = 5
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'win.html'
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+	
+}, 2000)
+
+function posicaoRandomica() {
+
+
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+
+		//console.log('elemento selecionado foi: v' + vidas)
+		if(vidas < 1) {
+
+			window.location.href = 'gameover.html'
+		} else {
+			document.getElementById('v' + vidas).src = "../img/coracao_vazio.png"
+
+			vidas--
+		}
+	}
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//criar o elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = '../img/mosca.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
+	document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
+
+	}
+}
+
